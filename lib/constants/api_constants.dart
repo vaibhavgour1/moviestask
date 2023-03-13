7f65ba6775a4288530a9e15b078376255c837c4e
Vaibhav <Vaibhav10gour99@gmail.com>
2023-03-13 23:46:35 +0530

    second

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "67af5e631dcbb4d0981b06996fcd47bc";
+const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
